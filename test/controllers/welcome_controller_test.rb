abe9e6fde58205650d1ffa7bd0c239cd06e72322
Tim Fleming <tfleming26@hotmail.com>
2021-09-17 17:27:51 -0600

    Install windi css and make changes to welcome page text

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get the welcome page" do
   	get "/"
-  	assert_select "h1", "Welcome to Gullsy! Caw . . . Caw, Caw!"
+  	assert_select "h1", "Welcome to Gullsy. Caw Caw!"
   	assert_response :success
   end
 end

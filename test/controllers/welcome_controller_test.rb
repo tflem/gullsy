ecd6b12aee40021571a91c13b69c6711ed989a14
Tim Fleming <tfleming26@hotmail.com>
2021-09-18 13:08:54 -0600

    Add page title and page title assertion

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -3,7 +3,8 @@ require "test_helper"
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get the welcome page" do
   	get "/"
-  	assert_select "h1", "Welcome to Gullsy. Caw Caw!"
-  	assert_response :success
+    assert_response :success
+    assert_select "title", "Home | Gullsy - An Alternative Bird App"
+  	assert_select "h1", "Welcome to Gullsy. Caw Caw!"  	
   end
 end

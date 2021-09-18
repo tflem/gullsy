3f9d17588b35078df920c0e1a9419982316d7d5e
Tim Fleming <tfleming26@hotmail.com>
2021-09-18 14:32:12 -0600

    Set the root route

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get the welcome page" do
-  	get "/"
+  	#get root_url
     assert_response :success
     assert_select "title", "Home | Gullsy - An Alternative Bird App"
   	assert_select "h1", "Welcome to Gullsy. Caw Caw!"  	

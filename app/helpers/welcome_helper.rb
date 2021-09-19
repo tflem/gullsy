d018cc01a524928fb97f4ec84afffd3dab0eff4e
Tim Fleming <tfleming26@hotmail.com>
2021-09-19 16:43:35 -0600

    Create custom title helper

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,8 @@
 module WelcomeHelper
+	# Returns full title based on current page
+	def full_title(page_title = "")
+		base_title = "Gullsy - An Alternative Bird App"
+		complete_title = "#{page_title} | #{base_title}"
+		page_title.empty? ? base_title : complete_title
+	end
 end
--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -2,9 +2,9 @@ require "test_helper"
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get the welcome page" do
-  	#get root_url
+  	get root_url
     assert_response :success
-    assert_select "title", "Home | Gullsy - An Alternative Bird App"
+    assert_select "title", "Gullsy - An Alternative Bird App"
   	assert_select "h1", "Welcome to Gullsy. Caw Caw!"  	
   end
 end

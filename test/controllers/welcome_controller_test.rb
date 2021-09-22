ca64fca3992080465e40509c24778c5042f1825b
Tim Fleming <tfleming26@hotmail.com>
2021-09-21 18:01:03 -0600

    Update app with named routes and links

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get the welcome page" do
-  	get root_url
+  	get root_path
     assert_response :success
     assert_select "title", "Gullsy - An Alternative Bird App"
   	assert_select "h1", "Welcome to Gullsy. Caw Caw!"  	

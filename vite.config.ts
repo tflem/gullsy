ed0f65e37cea71bfc13b2fa80d9367418b396bef
Tim Fleming <tfleming26@hotmail.com>
2021-09-24 16:46:09 -0600

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,22 @@ import RubyPlugin from "vite-plugin-ruby";
 import WindiCSS from "vite-plugin-windicss";
 
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
   plugins: [
     RubyPlugin(),
     WindiCSS({

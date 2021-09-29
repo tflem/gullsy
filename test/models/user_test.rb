37dd42d60ec09e0ab75a2a965951c90de519a7ad
Tim Fleming <tfleming26@hotmail.com>
2021-09-28 18:42:44 -0600

    Add validations for name and email fields

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Patrick Star", email: "pstar@bb.com")
+  end
+
+  test "should be a valid user" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
 end

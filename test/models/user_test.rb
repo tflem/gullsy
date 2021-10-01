4438c67856286afa23db25bd1379aaf333c77a6e
Tim Fleming <tfleming26@hotmail.com>
2021-09-30 18:02:16 -0600

    Validate email format with regex

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -28,4 +28,22 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "p" * 249 + "@bb.com"
     assert_not @user.valid?
   end
+
+  test "email validations should except valid email addresses" do
+    valid_addresses = %w[user@bb.com USER@bookie.COM A_US-ER@boo.bar.org
+                         first.last@boo.jp alice+bob@koobaz.cn]
+    valid_addresses.each do | valid_email |
+      @user.email = valid_email
+      assert @user.valid?, "#{valid_email.inspect} should be valid."
+    end
+  end
+
+  test "email validations should reject valid email addresses" do
+    invalid_addresses = %w[user@bb,com user_at_books.org user.name@bb.
+                           booki@boo_bazzle.com buki@far+faz.com bahh@bazzle..org]
+    invalid_addresses.each do |invalid_email|
+      @user.email = invalid_email
+      assert_not @user.valid?, "#{invalid_email.inspect} should be invalid."
+    end
+  end
 end

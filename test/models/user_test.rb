cd5a40b4e8c1234c4c1ac29530df0f6dc68c113c
Tim Fleming <tfleming26@hotmail.com>
2021-10-10 18:29:12 -0600

    Add password validations for length

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,38 +2,38 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Patrick Star", email: "pstar@bb.com",
-                     password: "patrick", password_confirmation: "patrick")
-  end
-
-  test "should be a valid user" do
-    assert @user.valid?
-  end
+    @user = User.new(name: "Patrick Star", email: "pstar@bb.com", 
+                     password: "hamburger", password_confirmation: "hamburger")
+  end  
 
   test "name should be present" do
-    @user.name = ""
-    assert_not @user.valid?
+    @user.name = " "
+    @user.valid?
+    assert_includes @user.errors.messages[:name], "can't be blank"
   end
 
   test "email should be present" do
-    @user.email = ""
-    assert_not @user.valid?
+    @user.email = " "
+    @user.valid?
+    assert_includes @user.errors.messages[:email], "can't be blank"
   end
 
   test "name should not exceed 50 characters" do
     @user.name = "p" * 51
-    assert_not @user.valid?
+    @user.valid?
+    assert_includes @user.errors.messages[:name], "is too long (maximum is 50 characters)"
   end
 
   test "email should not exceed 255 characters" do
     @user.email = "p" * 249 + "@bb.com"
-    assert_not @user.valid?
+    @user.valid?
+    assert_includes @user.errors.messages[:email], "is too long (maximum is 255 characters)"
   end
 
   test "email validations should except valid email addresses" do
     valid_addresses = %w[user@bb.com USER@bookie.COM A_US-ER@boo.bar.org
                          first.last@boo.jp alice+bob@koobaz.cn]
-    valid_addresses.each do | valid_email |
+    valid_addresses.each do |valid_email|
       @user.email = valid_email
       assert @user.valid?, "#{valid_email.inspect} should be valid."
     end
@@ -58,5 +58,17 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "PaTRickStaR@Bb.Com"
     @user.save
     assert_equal @user.email.downcase, @user.reload.email
+  end
+
+  test "password should be present" do
+    @user.password = @user.password_confirmation = " " * 8
+    @user.valid?
+    assert_includes @user.errors.messages[:password], "can't be blank"
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "p" * 7
+    @user.valid?
+    assert_includes @user.errors.messages[:password], "is too short (minimum is 8 characters)"
   end
 end
